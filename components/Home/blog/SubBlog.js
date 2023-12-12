b115861da9b22a05d26de48dc09f21f902a51d1f
Joshuamjv2 <joshuamjv22@gmail.com>
2023-12-12 13:13:45 +0300

    pointer for other blogs

--- components/Home/blog/SubBlog.js
+++ components/Home/blog/SubBlog.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function SubBlog({title, image, article = "Lorem ipsum dolor sit, amet consectetur adipisicing elit."}){
     return(
-        <div className="flex h-32 gap-2 hover:scale-105 hover:duration-300">
+        <div className="flex h-32 gap-2 hover:scale-105 hover:duration-300 cursor-pointer">
             <Image src={image} alt={title} className="w-1/2" />
             <div>
                 <h3 className="uppercase text-sm font-bold">Title</h3>
